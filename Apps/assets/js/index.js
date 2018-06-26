5276d0a5ad7a3562ff675b8a06173709752337d2
ApolloRisky <quoccuong.nguyen265@hotmail.com>
2018-06-26 12:59:14 +0700

    Add auto-resize productView

--- Apps/assets/js/index.js
+++ Apps/assets/js/index.js
@@ -23,4 +23,29 @@ $(document).ready(function () {
         $('ul.navbar-nav li.active').removeClass('active');
         $(this).addClass('active');
     })
+    //Login button pressed
+    $('#loginBtn').on('click',function(){
+        window.location.href = 'login.html';
+    });
+    //Handling sizing window
+    var $productView = $('.productView');
+    var $listItem = $('.productView > div');
+    var $firstItem = $listItem.first();
+
+    var hanldWindowSizing = function(){
+        var viewWidth = parseInt($productView.css('width'),10);
+        var itemWidth = parseInt($firstItem.css('width'),10);
+
+        var itemsPerRow = Math.floor(viewWidth/(itemWidth + parseInt($firstItem.css('padding-left'),10)));
+
+        //set height for $productView
+        var maxItemPerCol = Math.ceil($listItem.length/itemsPerRow) ;
+
+        var viewHeight = maxItemPerCol * (parseInt($firstItem.css('height'),10) + parseInt($firstItem.css('margin-top'),10));
+        $productView.css('height',viewHeight + parseInt($productView.css('padding-bottom'),10));
+    }
+    hanldWindowSizing();
+    $(window).on('resize',function(){
+        hanldWindowSizing();
+    })
 });
