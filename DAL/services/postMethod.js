c1e8c96a519fa0a09a73f6ec82d2853e9bed2008
dattran97 <tuandat1079@gmail.com>
2018-06-27 10:51:30 +0700

    remove login handler in DAL

--- DAL/services/postMethod.js
+++ DAL/services/postMethod.js
@@ -1,22 +1,7 @@
 const fs = require('fs');
 const parser = require('xml2js');
-var getMethod = require('./getMethod')
 
-login = (user) => {
-  let { email, password } = user;
-
-  return new Promise((resolve, reject) => {
-    let temp = getMethod.getListUser();
-    let users = JSON.parse(temp).NhanVien;
-    let user = users.filter((user) => {
-      return user.Email === email && user.Password === password;
-    });
-
-    if (user.length < 1) reject({'error': 'Email or password is incorrect'})
-    resolve(user[0])
-	})
-}
 
 module.exports = {
-  login
+  
 }
